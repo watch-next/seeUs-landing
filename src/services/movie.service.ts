a9738b5d8047bec1c06217949ce93e3d1256e23e
Rafael Pádua <145411939+rafaelPadua001@users.noreply.github.com>
2026-09-11 00:21:15 -0300

    fix: garph to rating movie

--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -33,6 +33,19 @@ export interface MovieDetail {
   genres: Genre[];
 }
 
+export interface Video {
+  id: string;
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  key: string;
+  site: string;
+  size: number;
+  type: string;
+  official: boolean;
+  published_at: string;
+}
+
 export interface WatchProvider {
   id: number;
   tmdb_id?: number;
